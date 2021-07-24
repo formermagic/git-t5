e0391f2bdee455da19eeb4f44b807cd0373b3ece
mozharovsky <mozharovsky@live.com>
2021-07-25 02:25:24 +0300

    refactor(data): export renamed data collator class

--- git_t5/data/__init__.py
+++ git_t5/data/__init__.py
@@ -1,2 +1,2 @@
-from .data_collator import FlaxDataCollatorForT5MLM
+from .data_collator import DataCollatorForT5MLM
 from .data_preprocessing import compute_input_and_target_lengths, prepare_dataset
