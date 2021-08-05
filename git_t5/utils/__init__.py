a280e5b4e5d3f2c8a31284f632c53f504a0683ac
mozharovsky <mozharovsky@live.com>
2021-08-05 15:00:09 +0300

    feat(utils): export `resolve_object` function

--- git_t5/utils/__init__.py
+++ git_t5/utils/__init__.py
@@ -1,4 +1,5 @@
 from .code_tokenizer import PyTokenizer, Tokenizer
 from .distributed import rank_zero_only
 from .html_stripper import HTMLStripper
+from .resolvers import resolve_object
 from .unidiff_builder import UnidiffBuilder
