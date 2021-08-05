0c9e956c13e8212c277240a3f55a00595922a3e4
mozharovsky <mozharovsky@live.com>
2021-08-05 15:02:04 +0300

    refactor(core): make data module a dataclass with `from_config` static builder

--- git_t5/core/data_module.py
+++ git_t5/core/data_module.py
@@ -59,7 +59,9 @@ class T5DataModuleConfig(DataModuleConfig):
     seed: int = MISSING  # derive from model config
 
 
+@dataclass
 class T5DataModule:
+    config: T5DataModuleConfig
     dataset: datasets.DatasetDict
     tokenizer: PreTrainedTokenizerBase
     data_collator: DataCollatorForT5MLM
@@ -68,27 +70,23 @@ class T5DataModule:
     target_length: int
     trainer: Optional[T5Trainer] = None
 
-    def __init__(self, config: T5DataModuleConfig) -> None:
-        self.config = config
+    @classmethod
+    def from_config(cls, config: T5DataModuleConfig) -> "T5DataModule":
+        tokenizer = cls.load_tokenizer(config)
+        max_sequence_length = config.max_sequence_length or VERY_LARGE_INTEGER
+        max_sequence_length = min(max_sequence_length, tokenizer.model_max_length)
 
-    def setup(self) -> None:
-        self.tokenizer = self.load_tokenizer()
-
-        self.max_sequence_length = self.config.max_sequence_length or VERY_LARGE_INTEGER
-        self.max_sequence_length = min(
-            self.max_sequence_length, self.tokenizer.model_max_length
-        )
-        self.input_length, self.target_length = compute_input_and_target_lengths(
-            self.max_sequence_length,
-            noise_density=self.config.mlm_probability,
-            mean_noise_span_length=self.config.mean_noise_span_length,
+        input_length, target_length = compute_input_and_target_lengths(
+            max_sequence_length,
+            noise_density=config.mlm_probability,
+            mean_noise_span_length=config.mean_noise_span_length,
             extra_tokens_per_span_inputs=1,
             extra_tokens_per_span_targets=1,
         )
 
-        eos_token_id = self.tokenizer.eos_token_id
-        pad_token_id = self.tokenizer.pad_token_id
-        sentinel_token_id = self.tokenizer.convert_tokens_to_ids("<extra_id_0>")  # type: ignore
+        eos_token_id = tokenizer.eos_token_id
+        pad_token_id = tokenizer.pad_token_id
+        sentinel_token_id = tokenizer.convert_tokens_to_ids("<extra_id_0>")  # type: ignore
         if eos_token_id is None:
             raise ValueError("Tokenizer must have an existing `eos_token_id` value.")
         if pad_token_id is None:
@@ -96,20 +94,30 @@ class T5DataModule:
         if sentinel_token_id is None:
             raise ValueError("Tokenizer must have an existing `eos_token_id` value.")
 
-        self.data_collator = DataCollatorForT5MLM(
-            tokenizer=self.tokenizer,
-            noise_density=self.config.mlm_probability,
-            mean_noise_span_length=self.config.mean_noise_span_length,
-            input_length=self.max_sequence_length,
-            target_length=self.target_length,
+        data_collator = DataCollatorForT5MLM(
+            tokenizer=tokenizer,
+            noise_density=config.mlm_probability,
+            mean_noise_span_length=config.mean_noise_span_length,
+            input_length=max_sequence_length,
+            target_length=target_length,
             eos_token_id=eos_token_id,
             pad_token_id=pad_token_id,
             sentinel_token_id=sentinel_token_id,
-            decoder_start_token_id=self.config.decoder_start_token_id,
+            decoder_start_token_id=config.decoder_start_token_id,
         )
 
-        self.dataset = self.load_dataset()
-        self.dataset = self.prepare_dataset(self.dataset)
+        dataset = cls.load_dataset(config)
+        dataset = cls.prepare_dataset(config, dataset, tokenizer, input_length)
+
+        return T5DataModule(
+            config,
+            dataset=dataset,
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            max_sequence_length=max_sequence_length,
+            input_length=input_length,
+            target_length=target_length,
+        )
 
     def train_dataloader(self) -> DataLoader:
         total_batch_size = self.config.train_batch_size * jax.device_count()
@@ -131,57 +139,65 @@ class T5DataModule:
             seed=None,
         )
 
-    def prepare_dataset(self, dataset: datasets.DatasetDict) -> datasets.DatasetDict:
-        if self.config.dataset_column is None:
+    @classmethod
+    def prepare_dataset(
+        cls,
+        config: T5DataModuleConfig,
+        dataset: datasets.DatasetDict,
+        tokenizer: PreTrainedTokenizerBase,
+        input_length: int,
+    ) -> datasets.DatasetDict:
+        if config.dataset_column is None:
             raise ValueError(
                 "You must provide a `dataset_column` to specify which column of the dataset to use."
             )
 
         dataset = prepare_dataset(
             dataset,
-            tokenize_fn(self.tokenizer, self.config.dataset_column),
-            input_length=self.input_length,
+            tokenize_fn(tokenizer, config.dataset_column),
+            input_length=input_length,
             batch_size=128,
-            load_from_cache_file=not self.config.overwrite_cache,
-            num_workers=self.config.num_workers,
+            load_from_cache_file=not config.overwrite_cache,
+            num_workers=config.num_workers,
         )
 
         return dataset
 
-    def load_dataset(self) -> datasets.DatasetDict:
-        if self.config.dataset_name is not None:
+    @classmethod
+    def load_dataset(cls, config: T5DataModuleConfig) -> datasets.DatasetDict:
+        if config.dataset_name is not None:
             dataset = datasets.load_dataset(
-                self.config.dataset_name,
-                self.config.dataset_config_name,
-                cache_dir=self.config.cache_dir,
+                config.dataset_name,
+                config.dataset_config_name,
+                cache_dir=config.cache_dir,
             )
 
             if not isinstance(dataset, datasets.DatasetDict):
                 dataset = datasets.DatasetDict(train=dataset)
 
             if "validation" not in dataset.keys():
-                valid_percentage = int(self.config.validation_size * 100)
+                valid_percentage = int(config.validation_size * 100)
                 dataset["validation"] = datasets.load_dataset(
-                    self.config.dataset_name,
-                    self.config.dataset_config_name,
+                    config.dataset_name,
+                    config.dataset_config_name,
                     split=f"train[:{valid_percentage}%]",
-                    cache_dir=self.config.cache_dir,
+                    cache_dir=config.cache_dir,
                 )
                 dataset["train"] = datasets.load_dataset(
-                    self.config.dataset_name,
-                    self.config.dataset_config_name,
+                    config.dataset_name,
+                    config.dataset_config_name,
                     split=f"train[{valid_percentage}%:]",
-                    cache_dir=self.config.cache_dir,
+                    cache_dir=config.cache_dir,
                 )
-        elif self.config.dataset_path is not None:
-            dataset = datasets.load_from_disk(self.config.dataset_path)
+        elif config.dataset_path is not None:
+            dataset = datasets.load_from_disk(config.dataset_path)
             if not isinstance(dataset, datasets.DatasetDict):
                 dataset = datasets.DatasetDict(train=dataset)
 
             if "validation" not in dataset.keys():
                 dataset = dataset["train"].train_test_split(
-                    test_size=self.config.validation_size,
-                    load_from_cache_file=not self.config.overwrite_cache,
+                    test_size=config.validation_size,
+                    load_from_cache_file=not config.overwrite_cache,
                 )
                 dataset["validation"] = dataset.pop("test")
         else:
@@ -189,18 +205,19 @@ class T5DataModule:
 
         return dataset
 
-    def load_tokenizer(self) -> PreTrainedTokenizerBase:
-        if self.config.tokenizer_path is not None:
+    @classmethod
+    def load_tokenizer(cls, config: T5DataModuleConfig) -> PreTrainedTokenizerBase:
+        if config.tokenizer_path is not None:
             tokenizer = AutoTokenizer.from_pretrained(
-                self.config.tokenizer_path,
-                cache_dir=self.config.cache_dir,
-                use_fast=self.config.use_fast_tokenizer,
+                config.tokenizer_path,
+                cache_dir=config.cache_dir,
+                use_fast=config.use_fast_tokenizer,
             )
-        elif self.config.model_path is not None:
+        elif config.model_path is not None:
             tokenizer = AutoTokenizer.from_pretrained(
-                self.config.model_path,
-                cache_dir=self.config.cache_dir,
-                use_fast=self.config.use_fast_tokenizer,
+                config.model_path,
+                cache_dir=config.cache_dir,
+                use_fast=config.use_fast_tokenizer,
             )
         else:
             raise ValueError(
