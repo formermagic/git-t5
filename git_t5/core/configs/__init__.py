c7c2b165632063c630b0a78ab74c9b7af24edea9
mozharovsky <mozharovsky@live.com>
2021-09-01 17:19:11 +0300

    feat(configs): export config types

---
+++ git_t5/core/configs/__init__.py
@@ -0,0 +1,8 @@
+from .dataset import (
+    DatasetConfig,
+    HFDatasetConfig,
+    LocalDatasetConfig,
+    MultitaskDatasetConfig,
+)
+from .tokenizer import PreTrainedTokenizer, PreTrainedTokenizerConfig
+from .training import TrainingConfig
