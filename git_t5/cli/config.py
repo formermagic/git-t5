33a59627107ba63da588a3dcfa470b28db8ffa5d
mozharovsky <mozharovsky@live.com>
2021-09-01 17:29:40 +0300

    feat(cli): register tokenizer, training and dataset base configs

--- git_t5/cli/config.py
+++ git_t5/cli/config.py
@@ -2,25 +2,30 @@ from dataclasses import dataclass
 
 from git_t5.core import (
     DataModuleConfig,
+    DatasetConfig,
     LoggerConfig,
     ModelConfig,
     OptimizerConfig,
+    PreTrainedTokenizerConfig,
     SchedulerConfig,
     TokenizerConfig,
     TokenizerTrainerConfig,
     TrainerConfig,
+    TrainingConfig,
 )
 from hydra.core.config_store import ConfigStore
 
 
 @dataclass
 class DefaultConfig:
-    tokenizer: TokenizerConfig = TokenizerConfig()
     tokenizer_trainer: TokenizerTrainerConfig = TokenizerTrainerConfig()
+    tokenizer: PreTrainedTokenizerConfig = PreTrainedTokenizerConfig()
+    dataset: DatasetConfig = DatasetConfig()
     model: ModelConfig = ModelConfig()
     data: DataModuleConfig = DataModuleConfig()
     optimizer: OptimizerConfig = OptimizerConfig()
     trainer: TrainerConfig = TrainerConfig()
+    training: TrainingConfig = TrainingConfig()
     logger: LoggerConfig = LoggerConfig()
 
 
@@ -28,14 +33,24 @@ def register_base_configs() -> None:
     cs = ConfigStore.instance()
     cs.store(name="default", node=DefaultConfig)
     cs.store(
-        group="tokenizer",
+        group="tokenizer_trainer",
+        name="base_tokenizer_trainer",
+        node=TokenizerTrainerConfig,
+    )
+    cs.store(
+        group="tokenizer_trainer/tokenizer",
         name="base_tokenizer",
         node=TokenizerConfig,
     )
     cs.store(
-        group="tokenizer_trainer",
-        name="base_tokenizer_trainer",
-        node=TokenizerTrainerConfig,
+        group="tokenizer",
+        name="base_pretrained_tokenizer",
+        node=PreTrainedTokenizerConfig,
+    )
+    cs.store(
+        group="dataset",
+        name="base_dataset",
+        node=DatasetConfig,
     )
     cs.store(
         group="model",
@@ -61,6 +76,11 @@ def register_base_configs() -> None:
         group="trainer",
         name="base_trainer",
         node=TrainerConfig,
+    )
+    cs.store(
+        group="training",
+        name="base_training",
+        node=TrainingConfig,
     )
     cs.store(
         group="logger",
