8a71d3c14ea586c3c95240c260a9241af86b700d
mozharovsky <mozharovsky@live.com>
2021-09-01 17:22:32 +0300

    feat(utils): export `stack_mappings` function

--- git_t5/utils/__init__.py
+++ git_t5/utils/__init__.py
@@ -1,4 +1,5 @@
 from .code_tokenizer import PyTokenizer, Tokenizer
+from .common import stack_mappings
 from .distributed import rank_zero_only
 from .html_stripper import HTMLStripper
 from .resolvers import resolve_object
