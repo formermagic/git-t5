05e7e3f39dd22a084f50e2943e309167a5f3e724
mozharovsky <mozharovsky@live.com>
2021-09-01 22:19:42 +0300

    feat(core): export main core types

---
+++ git_t5/__init__.py
@@ -0,0 +1,17 @@
+from git_t5.core import (
+    T5DataModule,
+    T5DataModuleConfig,
+    T5ModelForPreTraining,
+    T5ModelForPreTrainingConfig,
+    T5Trainer,
+    T5TrainerConfig,
+    TrainerConfig,
+)
+from git_t5.core.configs import (
+    DatasetConfig,
+    HFDatasetConfig,
+    LocalDatasetConfig,
+    MultitaskDatasetConfig,
+    PreTrainedTokenizerConfig,
+    TrainingConfig,
+)
