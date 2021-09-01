c0515e09d7f484b7522340cf591fdf81f052029d
mozharovsky <mozharovsky@live.com>
2021-09-01 17:19:42 +0300

    feat(configs): export single and multitask dataset types

--- git_t5/data/__init__.py
+++ git_t5/data/__init__.py
@@ -1,3 +1,3 @@
 from .data_collator import DataCollatorForT5MLM
-from .data_loader import DataLoader
 from .data_preprocessing import compute_input_and_target_lengths, prepare_dataset
+from .dataset import Dataset, T5Dataset, T5MultitaskDataset
