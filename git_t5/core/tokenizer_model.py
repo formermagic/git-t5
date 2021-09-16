7414156bc609ea44bceb94da1ea3b31a3a1432ce
mozharovsky <mozharovsky@live.com>
2021-09-16 18:03:07 +0300

    refactor(core): split tokenizer configuration into separate methods

--- git_t5/core/tokenizer_model.py
+++ git_t5/core/tokenizer_model.py
@@ -1,6 +1,6 @@
 import json
 from dataclasses import dataclass, field
-from typing import Iterator, List, Optional, Union
+from typing import Dict, Iterator, List, Optional, Union
 
 from tokenizers import (
     AddedToken,
@@ -9,18 +9,44 @@ from tokenizers import (
     decoders,
     normalizers,
     pre_tokenizers,
+    processors,
     trainers,
 )
 from tokenizers.implementations.base_tokenizer import BaseTokenizer
 from tokenizers.models import BPE
 from tokenizers.processors import TemplateProcessing
+from typing_extensions import TypedDict
 
 Token = Union[str, AddedToken]
 
 
+NORMALIZERS = {
+    "nfc": normalizers.NFC,
+    "nfd": normalizers.NFD,
+    "nfkc": normalizers.NFKC,
+    "nfkd": normalizers.NFKD,
+}
+
+
+def unicode_normalizer(normalizer: str) -> normalizers.Normalizer:
+    if normalizer not in NORMALIZERS:
+        raise ValueError(
+            "{} is not a known unicode normalizer. Available are {}".format(
+                normalizer, NORMALIZERS.keys()
+            )
+        )
+
+    return NORMALIZERS[normalizer]()
+
+
+class SpecialToken(TypedDict):
+    id: int
+    token: str
+
+
 @dataclass
 class TokenizerConfig:
-    vocab_size: int = 30_000
+    vocab_size: int = 32_000
     special_tokens: List[str] = field(default_factory=list)
     additional_special_tokens: List[str] = field(default_factory=list)
     model_max_length: Optional[int] = None
@@ -47,45 +73,18 @@ class SentencePieceTokenizer(BaseTokenizer):
         self.config = config
 
         # sentencepiece special tokens map
-        self.special_tokens_map = {
+        self.special_tokens_map: Dict[str, SpecialToken] = {
             "pad": {"id": 0, "token": config.pad_token},
             "eos": {"id": 1, "token": config.eos_token},
             "unk": {"id": 2, "token": config.unk_token},
         }
 
         # sentencepiece special tokens list
-        self.special_tokens: List[Token] = [""] * len(self.special_tokens_map)
-        for token_dict in self.special_tokens_map.values():
-            self.special_tokens[token_dict["id"]] = token_dict["token"]
-
+        self.special_tokens = self.get_special_tokens()
         self.special_tokens += list(config.special_tokens)
 
-        # sentencepiece byte-level bpe tokenizer
-        tokenizer = Tokenizer(BPE(dropout=config.dropout, unk_token=config.unk_token))
-        # original sentencepiece normalization
-        tokenizer.normalizer = normalizers.Sequence(  # type: ignore
-            [
-                normalizers.Nmt(),
-                normalizers.NFKC(),
-                normalizers.Replace(Regex(" {2,}"), " "),
-            ]
-        )
-        # byte-level pre-tokenization similar to GPT-2
-        tokenizer.pre_tokenizer = pre_tokenizers.ByteLevel(  # type: ignore
-            add_prefix_space=config.add_prefix_space
-        )
-        # byte-level decoding similar to GPT-2
-        tokenizer.decoder = decoders.ByteLevel()  # type: ignore
-        # original sentencepiece post processing
-        tokenizer.post_processor = TemplateProcessing(  # type: ignore
-            single=f"$A {self.special_tokens_map['eos']['token']}",
-            special_tokens=[
-                (
-                    self.special_tokens_map["eos"]["token"],
-                    self.special_tokens_map["eos"]["id"],
-                )
-            ],
-        )
+        # sentencepiece bpe tokenizer
+        tokenizer = self.get_tokenizer()
 
         parameters = {
             "model": "ByteLevelBPE",
@@ -95,6 +94,60 @@ class SentencePieceTokenizer(BaseTokenizer):
         }
 
         super().__init__(tokenizer, parameters)
+
+    def get_special_tokens(self) -> List[Token]:
+        special_tokens: List[Token] = [""] * len(self.special_tokens_map)
+        for token_dict in self.special_tokens_map.values():
+            special_tokens[token_dict["id"]] = token_dict["token"]
+        return special_tokens
+
+    def get_tokenizer(self) -> Tokenizer:
+        # configure the sentencepiece bpe tokenizer
+        tokenizer = Tokenizer(
+            BPE(
+                dropout=self.config.dropout,
+                unk_token=self.config.unk_token,
+            )
+        )
+
+        tokenizer.normalizer = self.get_normalizer()  # type: ignore
+        tokenizer.pre_tokenizer = self.get_pre_tokenizer()  # type: ignore
+        tokenizer.decoder = self.get_decoder()  # type: ignore
+        tokenizer.post_processor = self.get_post_processor()  # type: ignore
+
+        return tokenizer
+
+    def get_normalizer(self) -> normalizers.Normalizer:
+        normalizer_list: List[normalizers.Normalizer] = []
+        normalizer_list.append(normalizers.Nmt())
+
+        if self.config.unicode_normalizer is not None:
+            normalizer_list.append(unicode_normalizer(self.config.unicode_normalizer))
+
+        if self.config.remove_extra_spaces:
+            normalizer_list.append(normalizers.Replace(Regex(" {2,}"), " "))
+
+        if self.config.lowercase:
+            normalizer_list.append(normalizers.Lowercase())
+
+        return normalizers.Sequence(normalizer_list)  # type: ignore
+
+    def get_pre_tokenizer(self) -> pre_tokenizers.PreTokenizer:
+        return pre_tokenizers.ByteLevel()
+
+    def get_decoder(self) -> decoders.Decoder:
+        return decoders.ByteLevel()
+
+    def get_post_processor(self) -> processors.PostProcessor:
+        return TemplateProcessing(  # type: ignore
+            single=f"$A {self.special_tokens_map['eos']['token']}",
+            special_tokens=[
+                (
+                    self.special_tokens_map["eos"]["token"],
+                    self.special_tokens_map["eos"]["id"],
+                )
+            ],
+        )
 
     def train_from_files(self, files: Union[str, List[str]]) -> None:
         """Train the model using the given files"""
